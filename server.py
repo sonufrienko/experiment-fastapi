20d5d017234d8a798bdb1a66b302c83ec876463b
Sergey Onufrienko <sergey@onufrienko.com>
2020-08-02 11:53:54 +0300

    Import routers

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
-from typing import Optional
+import uvicorn
 from fastapi import FastAPI
 
+from routers import countries, storage, healthcheck
+from config import settings
 
 app = FastAPI()
+app.include_router(countries.router, prefix="/v1")
+app.include_router(storage.router, prefix="/v1")
+app.include_router(healthcheck.router)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+def startup_event():
+    # write logs
+    pass
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-    
+@app.on_event("shutdown")
+def shutdown_event():
+    # write logs
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=settings.SERVER_HOST, port=int(settings.SERVER_PORT))
